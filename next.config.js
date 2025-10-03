8e0c1e5b5a75bc371b8b3a525bcc08301b66d216
anir1977 <yuba1977@gmail.com>
2025-10-04 00:35:10 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  // (اختياري)
-  // typescript: { ignoreBuildErrors: true },
-  // eslint: { ignoreDuringBuilds: true },
-};
-module.exports = nextConfig;
