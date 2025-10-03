87ca62802d50099af0f1559ab4816e92969bcc9b
anir1977 <yuba1977@gmail.com>
2025-10-03 23:42:07 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Ben Daoud Bijouterie</h1>
+      <p>Site déployé avec Next.js (static export) sur Vercel.</p>
+    </main>
+  );
+}
