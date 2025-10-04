a020f343d4111394430c22925502aa48c9c463a3
anir1977 <yuba1977@gmail.com>
2025-10-04 01:33:39 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 // app/page.tsx
-import { redirect } from 'next/navigation';
-
-export default function Accueil() {
-  // تحويل تلقائي نحو صفحة "bijoux" أو أي صفحة رئيسية تريدها
-  redirect('/bijoux'); 
-  return null;
-}
+export { default } from './bijoux/page';
+export { metadata } from './bijoux/page';
